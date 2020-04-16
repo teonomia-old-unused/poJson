9af1d530e0ab7f2184870077b2dabddb0828458a
alvaroseparovich <alvasepro@gmail.com>
2020-04-15 21:26:40 -0300

    FEAT: parse html to poJson

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const F = require('./File')
+const HTML = require('node-html-parser')
 
 async function searchAndTransform (print =true , path = `./gettext_po_sample_file.po`){
   const file = await F.rf(path)
@@ -14,6 +15,10 @@ async function searchAndTransform (print =true , path = `./gettext_po_sample_fil
 function transform (string){
   const splitedPo = string.split('\n\n')
 
+  function breakLine (string) {
+    return string.split('\\n"\n"')
+  }
+
   const poJson = {
     header: splitedPo.shift().split('\n"'),
     body: splitedPo.map(i => {
@@ -21,18 +26,49 @@ function transform (string){
       const comment = splited.shift()
       const msg = splited[0].split('msgstr "')
       return {
-        id: msg[0],
-        str: msg[1],
+        id: breakLine(msg[0]),
+        str: msg[1] ? breakLine(msg[1]) : msg[1],
         comment
       }
     })
   }
   // console.log(poJson)
-  // console.log(poJson.body.map(i=>i.id))
+  console.log(poJson.body.map(i=>i.id))
   return poJson
+}
+
+function html2poJson (string) {
+  // console.log(string)
+  const content = HTML.parse(string)
+  console.log(content.childNodes[0].rawText)
+
+  const body = content.childNodes[0].childNodes.map(node => {
+    return {
+      id: node.rawText,
+      msg: '',
+      comment: `##HTML: <${node.tagName} ${node.rawAttrs}>{{#c}}</${node.tagName}>` }
+    }
+  )
+  return JSON.stringify({
+    header: [
+      "msgid \"\"\nmsgstr \"\"",
+      "MIME-Version: 1.0\\n\"",
+      "Content-Type: text/plain; charset=UTF-8\\n\"",
+      "Content-Transfer-Encoding: 8bit\\n\"",
+      `X-Generator: poJson 0.0.1\\n\"`,
+      "Project-Id-Version: \\n\"",
+      "Language: pt-br\\n\"",
+      "POT-Creation-Date: \\n\"",
+      "PO-Revision-Date: \\n\"",
+      "Last-Translator: \\n\"",
+      "Language-Team: \\n\""
+    ],
+    body
+  })
 }
 
 module.exports = {
   transform,
-  searchAndTransform
+  searchAndTransform,
+  html2poJson
 }
--- src/run.js
+++ src/run.js
@@ -1,3 +1,12 @@
 const r = require('./index')
+const F = require('./File')
+async function  traslate (it) {
+  // const file = await r.searchAndTransform(false)
+  // await F.wf('po.json', JSON.stringify(file))
 
-r.searchAndTransform()
+  const html = await F.rf('./article.html')
+  const string = r.html2poJson(html.toString())
+  console.log(string)
+  F.wf('html.json', string)
+}
+traslate('')

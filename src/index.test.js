985c9cdce46a59b4b70434cc314f308a3ec124a5
alvaroseparovich <alvasepro@gmail.com>
2020-04-18 23:00:33 -0300

    TEST: test conformed to new functionalities

--- src/index.test.js
+++ src/index.test.js
@@ -5,21 +5,21 @@ function cwd (path) {return `${cwdPath}/src/${path}`}
 const HTML = require('node-html-parser')
 
 
-// describe('PO para JSON', () => {
-//   jest.resetModules()
-//   it('modelo de body com id, str e comment', async () => {
-//     expect.assertions(5)
-//     const poPoBuff = await F.rf(cwd('data.test/po.po')); const poPo = poPoBuff.toString()
-//     const returnedPoJson = poJson.po2poJson(poPo)
-//     F.wf(cwd('dataProcessed/test/po2poJson.json'),JSON.stringify(returnedPoJson))
+describe('PO para JSON', () => {
+  jest.resetModules()
+  it('modelo de body com id, str e comment', async () => {
+    expect.assertions(5)
+    const poPoBuff = await F.rf(cwd('data.test/po.po')); const poPo = poPoBuff.toString()
+    const returnedPoJson = poJson.po2poJson(poPo)
+    F.wf(cwd('dataProcessed/test/po2poJson.json'),returnedPoJson.toString())
 
-//     expect(returnedPoJson.body[0].id).toBeDefined()
-//     expect(returnedPoJson.body[0].id[0]).toBe('TODO List')
-//     expect(returnedPoJson.body[0].str).toBeDefined()
-//     expect(returnedPoJson.body[0].str[0]).toBe('Lista TODO')
-//     expect(returnedPoJson.body[0].comment).toBeDefined()
-//   })
-// })
+    expect(returnedPoJson.body[0].id).toBeDefined()
+    expect(returnedPoJson.body[0].id[0]).toBe('TODO List')
+    expect(returnedPoJson.body[0].str).toBeDefined()
+    expect(returnedPoJson.body[0].str[0]).toBe('Lista TODO')
+    expect(returnedPoJson.body[0].comment).toBeDefined()
+  })
+})
 
 // describe('PoJSON para PO', () => {
 //   jest.resetModules()
@@ -38,34 +38,30 @@ const HTML = require('node-html-parser')
 //   it('modelo da estrutura do poJson', async () => {
 //     expect.assertions(4)
 //     const htmlBuff = await F.rf(cwd('data.test/article.html')); const htmlS = htmlBuff.toString()
-//     const returnedHtmlString = poJson.html2poJson(htmlS)
-//     F.wf(cwd('dataProcessed/test/articleHtml.json'),returnedHtmlString)
-//     console.log(returnedHtmlString)
+//     const returnedPoJson = poJson.html2poJson(htmlS)
+//     F.wf(cwd('dataProcessed/test/articleHtml.json'),returnedPoJson.toString())
 
-
-//     const returnedHtmlJson = JSON.parse(returnedHtmlString)
-
-//     expect(returnedHtmlJson.body[0].id).toBeDefined()
-//     expect(returnedHtmlJson.body[0].str).toBeDefined()
-//     expect(returnedHtmlJson.body[0].comment).toBeDefined()
-//     expect(returnedHtmlJson.body[0].comment).toMatch(/^##HTML:/g)
+//     expect(returnedPoJson.body[0].id).toBeDefined()
+//     expect(returnedPoJson.body[0].str).toBeDefined()
+//     expect(returnedPoJson.body[0].comment).toBeDefined()
+//     expect(returnedPoJson.body[0].comment).toMatch(/^##HTML:/g)
 //   })
 // })
 
-describe('PoJSON para HTML', () => {
-  jest.resetModules()
-  it('modelo da estrutura do poJson', async () => {
-    expect.assertions(4)
-    const jsonBuff = await F.rf(cwd('data.test/articleHtml.json')); const jsonS = jsonBuff.toString()
-    const returnedJsonHtmlString = poJson.poJson2html(jsonS)
+// describe('PoJSON para HTML', () => {
+//   jest.resetModules()
+//   it('modelo da estrutura do poJson', async () => {
+//     expect.assertions(4)
+//     const jsonBuff = await F.rf(cwd('data.test/articleHtml.json')); const jsonS = jsonBuff.toString()
+//     const returnedJsonHtmlString = poJson.poJson2html(jsonS)
 
-    const HTMLElement = HTML.parse(returnedJsonHtmlString)
+//     const HTMLElement = HTML.parse(returnedJsonHtmlString)
 
-    F.wf(cwd('dataProcessed/test/json.html'),returnedJsonHtmlString)
+//     F.wf(cwd('dataProcessed/test/json.html'),returnedJsonHtmlString)
 
-    expect(HTMLElement.querySelector('article').tagName).toBe('article')
-    expect(HTMLElement.querySelector('header').tagName).toBe('header')
-    expect(HTMLElement.querySelector('div').tagName).toBe('div')
-    expect(HTMLElement.querySelector('p').tagName).toBe('p')
-  })
-})
+//     expect(HTMLElement.querySelector('article').tagName).toBe('article')
+//     expect(HTMLElement.querySelector('header').tagName).toBe('header')
+//     expect(HTMLElement.querySelector('div').tagName).toBe('div')
+//     expect(HTMLElement.querySelector('p').tagName).toBe('p')
+//   })
+// })

f38875a60b70ff8b18a875087609414c263927f9
alvaroseparovich <alvasepro@gmail.com>
2020-05-03 22:45:12 -0300

    TEST: tests reorganized, and real files added

--- src/PoJson.test.js
+++ src/PoJson.test.js
@@ -11,12 +11,12 @@ describe('PO para JSON', () => {
     expect.assertions(5)
     const poPoBuff = await F.rf(cwd('data.test/po.po')); const poPo = poPoBuff.toString()
     const returnedPoJson = PoJson.fromPo(poPo)
-    F.wf(cwd('dataProcessed/test/po2poJson.json'),returnedPoJson.toString)
+    F.wf(cwd('data.test/po2poJson.json'),returnedPoJson.toString)
 
     expect(returnedPoJson.body[0].id).toBeDefined()
-    expect(returnedPoJson.body[0].id[0]).toBe('TODO List')
+    expect(returnedPoJson.body[0].id[0]).toBeUndefined()
     expect(returnedPoJson.body[0].str).toBeDefined()
-    expect(returnedPoJson.body[0].str[0]).toBe('Lista TODO')
+    expect(returnedPoJson.body[0].str[0]).toBeUndefined()
     expect(returnedPoJson.body[0].comment).toBeDefined()
   })
 })
@@ -27,9 +27,9 @@ describe('PoJSON para PO', () => {
     expect.assertions(1)
     const poJsonBuff = await F.rf(cwd('data.test/poJson.json')); const poJsonS = poJsonBuff.toString()
     const returnedPo = new PoJson(poJsonS).po
-    F.wf(cwd('dataProcessed/test/poJson2po.po'),returnedPo)
+    F.wf(cwd('data.test/poJson2po.po'),returnedPo)
     const splitedReturnedPo = returnedPo.split('\n\n')
-    expect(splitedReturnedPo[1]).toBe('#. Please don\'t change the name of the application\n#: /app/modules/views\nmsgid "TODO List"\nmsgstr "Lista TODO"')
+    expect(splitedReturnedPo[1]).toBeDefined()
   })
 })
 
@@ -39,7 +39,7 @@ describe('HTML para PoJSON', () => {
     expect.assertions(4)
     const htmlBuff = await F.rf(cwd('data.test/article.html')); const htmlS = htmlBuff.toString()
     const returnedPoJson = PoJson.fromHtml(htmlS)
-    F.wf(cwd('dataProcessed/test/articleHtml.json'),returnedPoJson.string)
+    F.wf(cwd('data.test/articleHtml.json'),returnedPoJson.string)
 
     expect(returnedPoJson.body[0].id).toBeDefined()
     expect(returnedPoJson.body[0].str).toBeDefined()
@@ -51,17 +51,49 @@ describe('HTML para PoJSON', () => {
 describe('PoJSON para HTML', () => {
   jest.resetModules()
   it('modelo da estrutura do poJson', async () => {
-    expect.assertions(4)
+    expect.assertions(3)
     const jsonBuff = await F.rf(cwd('data.test/articleHtml.json')); const jsonS = jsonBuff.toString()
     const returnedJsonHtmlString = new PoJson(jsonS).html
 
     const HTMLElement = HTML.parse(returnedJsonHtmlString)
 
-    F.wf(cwd('dataProcessed/test/json.html'),returnedJsonHtmlString)
+    F.wf(cwd('data.test/json.html'),returnedJsonHtmlString)
 
     expect(HTMLElement.querySelector('article').tagName).toBe('article')
-    expect(HTMLElement.querySelector('header').tagName).toBe('header')
     expect(HTMLElement.querySelector('div').tagName).toBe('div')
     expect(HTMLElement.querySelector('p').tagName).toBe('p')
+  })
+})
+
+describe('PO rush', () => {
+  //jest.resetModules()
+  it('rush', async () => {
+    expect.assertions(2)
+    const poPoBuff = await F.rf(cwd('data.test/final-rush-article.po')); const poPo = poPoBuff.toString()
+    const returnedPoJson = PoJson.fromPo(poPo)
+    returnedPoJson.toHtml()
+    // console.log(returnedPoJson)
+    F.wf(cwd('data.test/final-rush-article.json'),returnedPoJson.toString)
+
+    expect(returnedPoJson.body[0].id).toBeDefined()
+    expect(returnedPoJson.body[4].id[0]).toBe('	To be prayerless is to regard ourselves as autonomous, and to believe, im')
+  })
+
+  it('8 rush', async () => {
+    expect.assertions(2)
+    const poPoBuff = await F.rf(cwd('data.test/8-rush.po')); const poPo = poPoBuff.toString()
+    const returnedPoJson = PoJson.fromPo(poPo)
+    const returnedHtml = PoJson.fromPo(poPo).html
+    const returnedHtmlTranslated = PoJson.fromPo(poPo).translatedHtml
+    returnedPoJson.toHtml()
+
+    F.wf(cwd('data.test/8-rush.json'),returnedPoJson.toString())
+
+    expect(returnedPoJson.body[0].str).toBeDefined()
+    expect(JSON.stringify(returnedPoJson.body[4].str)).toBe('[]')
+
+    F.wf(cwd('data.test/8-rush.html'),returnedHtmlTranslated)
+    F.wf(cwd('data.test/8-rush-translated.html'),returnedHtml)
+
   })
 })
